5b515272f7eee0fdab15aa96d20aef86cb7019e2
Kamel E.K <elkhantache.kamel@gmail.com>
2020-04-13 19:46:04 +0200

    mod6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,11 +3,14 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import {Observable} from 'rxjs/Observable';
 import {Album} from './album';
+import {Product} from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) {
   }
@@ -17,5 +20,17 @@ export class ProductService {
       .map(response => {
       return <Album>response.json();
     });
+  }
+
+  getProducts(): Observable<Product[]> {
+    console.log('getProducts');
+
+    return this._http.get(this._productsUrl)
+      .map(response => {
+        console.log('response' + response);
+        return <Product[]>response.json();
+      }, error => {
+        console.log('error' + error)
+      });
   }
 }
